2619cb87cc5b7dc17733aed308dfb0e16cdb4fff
Dimitri Mansour <dimi.mansour03@gmail.com>
2023-11-22 04:58:09 -0500

    pdtr

---
+++ assets/lolptdr.js
@@ -0,0 +1,2 @@
+var element = document.getElementsByClassName("fixed left-0 right-0 top-0 z-20 min-h-[150px] bg-gradient-to-b from-denim-300 via-denim-300 to-transparent sm:from-transparent");
+element.remove();
