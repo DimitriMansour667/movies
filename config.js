5e2e5eab312cd3014437d3cf5d3fc1a0cf512d6b
unknown <dimi.mansour03@gmail.com>
2023-11-22 03:22:17 -0500

    test

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://simple-proxy.dimi-mansour03.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNGI2YjI3OGI4Y2ZkMmIwNWM5NjhkODQ1MDI1ZjZjNSIsInN1YiI6IjY1NWRiODA2ZTk0MmVlMDBlMTZlMWY0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NEx0Y3hap6ICTUO7jk9JxIqPRvWBdYIHzi6kR8oNk4E"
+};
