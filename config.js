b16fb1a559ce6288d36cfd446e8bc2d54a88a1a0
Dimitri Mansour <dimi.mansour03@gmail.com>
2024-01-22 09:27:07 -0500

    bagay2

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "CHANGEME",
+  VITE_CORS_PROXY_URL: "https://simple-proxy.dimi-mansour03.workers.dev/",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY:
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNzMwODU5MDM2ZGQ3YzM4NjAwMzMyNWQ1Zjk0NWE4ZiIsInN1YiI6IjY1NWRiODA2ZTk0MmVlMDBlMTZlMWY0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4rs8N23AZs4k51frtPhjvFeotqK89BVc7cBXcbvOtSI",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
@@ -15,5 +16,5 @@ window.__CONFIG__ = {
   VITE_BACKEND_URL: null,
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
-  VITE_DISALLOWED_IDS: ""
+  VITE_DISALLOWED_IDS: "",
 };
